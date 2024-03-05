42608f9e72e9209e75e2d3b19653ac473db845db
Pablo Zarandona De Juan <pazarand@c3r5s3.42urduliz.com>
2024-03-05 18:05:00 +0100

    05-03-24(2)

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,31 @@
-#ifndef GET_NEXT_LINE2_H
- #define GET_NEXT_LINE2_H
- #ifndef BUFFER_SIZE
- #define BUFFER_SIZE 42
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pazarand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 17:40:36 by pazarand          #+#    #+#             */
+/*   Updated: 2024/03/05 17:53:13 by pazarand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
- #endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 13
 
- #include <stdlib.h>
- #include <unistd.h>
- #include <fcntl.h>
- #include <stdio.h>
+# endif
 
-char *get_next_line(int fd);
-char *append_char_to_string(char *str, char c);
-int process_buffer(char *buffer, int *buffer_index, char **line);
-ssize_t try_read_from_file(int fd, char *buffer, int *buffer_index);
-char *finalize_and_return_line(ssize_t bytes_read, char **line);
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*append_char_to_string(char *str, char c);
+int		process_buffer(char *buffer, int *buffer_index, char **line);
+ssize_t	try_read_from_file(int fd, char *buffer, int *buffer_index);
+char	*finalize_and_return_line(ssize_t bytes_read, char **line);
 
 #endif
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,93 +6,92 @@
 /*   By: pazarand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:38:32 by pazarand          #+#    #+#             */
-/*   Updated: 2024/03/05 14:34:16 by pazarand         ###   ########.fr       */
+/*   Updated: 2024/03/05 17:39:04 by pazarand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *append_char_to_string(char *str, char c)
+char	*append_char_to_string(char *str, char c)
 {
-    int     length;
-    int     i;
-    char    *new_str;
+	int		length;
+	int		i;
+	char	*new_str;
 
 	length = 0;
 	if (str != NULL)
-    {
-        while (str[length])
-            length++;
-    }
-    new_str = malloc(sizeof(char) * (length + 2));
-    if (!new_str)
-        return (NULL);
-    i = 0;
-    while (i < length)
-    {
-        new_str[i] = str[i];
-        i++;
-    }
-    new_str[length] = c;
-    new_str[length + 1] = '\0';
-    if (str != NULL)
-        free(str);
-    return (new_str);
+	{
+		while (str[length])
+			length++;
+	}
+	new_str = malloc(sizeof(char) * (length + 2));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[length] = c;
+	new_str[length + 1] = '\0';
+	if (str != NULL)
+		free(str);
+	return (new_str);
 }
 
-
-int process_buffer(char *buffer, int *buffer_index, char **line)
+int	process_buffer(char *buffer, int *buffer_index, char **line)
 {
-    int     i;
-    int     line_completed;
-    char    *temp_line;
+	int		i;
+	int		line_completed;
+	char	*temp_line;
 
-    i = *buffer_index;
-    line_completed = 0;
-    while (buffer[i] != '\0')
-    {
+	i = *buffer_index;
+	line_completed = 0;
+	while (buffer[i] != '\0')
+	{
 		temp_line = append_char_to_string(*line, buffer[i]);
-        if (!temp_line)
-            return (-1);
-        *line = temp_line;
-        if (buffer[i++] == '\n')
-        {
-            line_completed = 1;
-            break;
-        }
-    }
-    if (buffer[i] == '\0')
-    {
-        *buffer_index = 0;
-    }
-    else
-        *buffer_index = i;
-    return (line_completed);
+		if (!temp_line)
+			return (-1);
+		*line = temp_line;
+		if (buffer[i++] == '\n')
+		{
+			line_completed = 1;
+			break ;
+		}
+	}
+	if (buffer[i] == '\0')
+	{
+		*buffer_index = 0;
+	}
+	else
+		*buffer_index = i;
+	return (line_completed);
 }
 
-ssize_t try_read_from_file(int fd, char *buffer, int *buffer_index)
+ssize_t	try_read_from_file(int fd, char *buffer, int *buffer_index)
 {
-    ssize_t bytes_read;
+	ssize_t	bytes_read;
 
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    if (bytes_read <= 0)
-    {
-        buffer[0] = '\0';
-        *buffer_index = 0;
-        return (bytes_read);
-    }
-    buffer[bytes_read] = '\0';
-    return (bytes_read);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		buffer[0] = '\0';
+		*buffer_index = 0;
+		return (bytes_read);
+	}
+	buffer[bytes_read] = '\0';
+	return (bytes_read);
 }
 
-char    *finalize_and_return_line(ssize_t bytes_read, char **line)
+char	*finalize_and_return_line(ssize_t bytes_read, char **line)
 {
-    if (bytes_read == -1
-        || (bytes_read == 0 && (*line == NULL || **line == '\0')))
-    {
-        if (*line != NULL)
-            free(*line);
-        return (NULL);
-    }
-    return (*line);
+	if (bytes_read == -1
+		|| (bytes_read == 0 && (*line == NULL || **line == '\0')))
+	{
+		if (*line != NULL)
+			free(*line);
+		return (NULL);
+	}
+	return (*line);
 }
